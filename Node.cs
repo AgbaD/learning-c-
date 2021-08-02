de20478a4727d32456c749297da8574b881b05fb
BlankGodd <blankgodd33@gmail.com>
2021-08-02 18:35:56 +0100

    k

---
+++ Node.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_csharp
+{
+    class Node
+    {
+        public int data;
+        public bool next;
+    }
+}
