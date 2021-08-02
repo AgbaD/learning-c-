e36d0045d25e9d071522fcd9c34e4b5539b67ee7
BlankGodd <blankgodd33@gmail.com>
2021-08-02 13:29:09 +0100

    starting out

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learning_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
